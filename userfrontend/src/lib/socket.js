44b1cb60896a30327c5a7e91b68c5648560d20b6
Nikhil Kumar <barnawalmanoj000@gmail.com>
2025-12-04 15:23:52 +0530

    Socket.js config updated

--- userfrontend/src/lib/socket.js
+++ userfrontend/src/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 export const socket = io(
-  import.meta.env.DEV ? "http://localhost:3000" : "https://your-server.com",
+  import.meta.env.DEV ? "http://localhost:3000" : "https://torobackend-8kmx.onrender.com/",
   { transports: ["websocket"] }
 );
